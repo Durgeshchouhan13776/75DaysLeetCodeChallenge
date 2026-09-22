1bf4faaa4fee520da3384eca52dc54f0705ff26d
Durgesh Chouhan <durgeshchouhan13776@gmail.com>
2026-09-22 22:34:15 +0530

    Time: 40 ms (7.73%), Space: 48.2 MB (5.2%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,43 @@
+// class Solution {
+//      TLE aayega
+//     public int coinChange(int[] coins, int amount) {
+//         int ans = (int) coinCount(0,coins,amount);
+//         if(ans==Integer.MAX_VALUE) return -1;
+//         return ans;
+//     }
+//     public long coinCount(int i, int[] coins, int amount){
+//         if(i==coins.length){
+//             if(amount==0) return 0;
+//             else return Integer.MAX_VALUE;
+//         }
+//         long skip = coinCount(i+1,coins,amount);
+//         if(amount-coins[i]<0) return skip;
+//         long pick =1+coinCount(i,coins,amount-coins[i]);
+//         return Math.min(pick,skip);
+//     }
+// }
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        long[][] dp = new long[coins.length][amount+1];
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<dp[0].length; j++){
+                dp[i][j] = -1;
+            }
+        }
+        int ans = (int) coinCount(0,coins,amount,dp);
+        if(ans==Integer.MAX_VALUE) return -1;
+        return ans;
+    }
+    public long coinCount(int i, int[] coins, int amount,long[][] dp){
+        if(i==coins.length){
+            if(amount==0) return 0;// no more coins needed
+            else return Integer.MAX_VALUE;// not a valid combination
+        }
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        long skip = coinCount(i+1,coins,amount,dp);
+        if(amount-coins[i]<0) return dp[i][amount] =  skip;
+        long pick =1+coinCount(i,coins,amount-coins[i],dp);
+        return dp[i][amount] = Math.min(pick,skip);
+    }
+}
