144771adda9f815eaa888e00c78206e9cdf725a4
Durgesh Chouhan <durgeshchouhan13776@gmail.com>
2026-09-23 18:14:17 +0530

    Time: 10 ms (52.97%), Space: 47.1 MB (10.44%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,45 @@
+// class Solution {
+//     public int findTargetSumWays(int[] nums, int target) {
+//         return ways(0,nums,target);
+//     }
+//     public int ways(int i,int[] nums, int target){
+//         if(i==nums.length){
+//             if(target==0) return 1;
+//             else return 0;
+//         }
+//         int add = ways(i+1,nums,target-nums[i]);
+//         int sub = ways(i+1,nums,target+nums[i]);
+
+//         return add+sub;
+//     }
+// }
+
+class Solution {
+    static int sum;
+    public int findTargetSumWays(int[] arr, int target) {
+
+        //i->0 to n-1 | res = -sum to sum
+        //dp[i][res+sum] -> ways(i,res)
+        sum=0; 
+        int n = arr.length;
+        for(int ele : arr) sum+=ele;
+        int[][] dp = new int[n][2*sum+1];
+        for(int i=0;i<dp.length; i++){
+            for(int j=0; j<dp[0].length; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return ways(0,arr,0,target,dp);
+    }
+    public int ways(int i,int[] arr,int res, int target,int[][] dp){
+        if(i==arr.length){
+            if(target==res) return 1;
+            else return 0;
+        }
+        if(dp[i][res+sum]!=-1) return dp[i][res+sum];
+        int add = ways(i+1,arr,res+arr[i],target,dp);
+        int sub = ways(i+1,arr,res-arr[i],target,dp);
+
+        return dp[i][res+sum] =  add+sub;
+    }
+}
